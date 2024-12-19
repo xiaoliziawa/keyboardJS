6c294d2c46283349ae022eeca859505eb58f4294
xiaoliziawa <3682822069@qq.com>
2024-12-19 22:36:07 +0800

    更新

--- src/main/java/net/prizowo/keyboardjs/keyboard/Keyboard.java
+++ src/main/java/net/prizowo/keyboardjs/keyboard/Keyboard.java
@@ -2,17 +2,47 @@ package net.prizowo.keyboardjs.keyboard;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.Minecraft;
+import org.lwjgl.glfw.GLFW;
 
-
-public class Keyboard {
-    public Keyboard() {} // 防止实例化
-
+public record Keyboard() {
+    /**
+     * 检查按键是否被按下
+     * @param key 要检查的按键
+     * @return 如果按键被按下返回true
+     */
     public static boolean pressed(KeyboardEventJS.Keys key) {
         long window = Minecraft.getInstance().getWindow().getWindow();
-        return InputConstants.isKeyDown(window, KeyMapping.getKeyCode(key.name()));
+        
+        if (key.name().startsWith("MOUSE_")) {
+            int mouseButton = getMouseButton(key.name());
+            if (mouseButton != -1) {
+                return GLFW.glfwGetMouseButton(window, mouseButton) == GLFW.GLFW_PRESS;
+            }
+        }
+        
+        return InputConstants.isKeyDown(window, key.ordinal());
     }
 
+    /**
+     * 检查按键是否被释放
+     * @param key 要检查的按键
+     * @return 如果按键被释放返回true
+     */
     public static boolean released(KeyboardEventJS.Keys key) {
         return !pressed(key);
+    }
+
+    private static int getMouseButton(String keyName) {
+        return switch (keyName) {
+            case "MOUSE_LEFT" -> GLFW.GLFW_MOUSE_BUTTON_LEFT;
+            case "MOUSE_RIGHT" -> GLFW.GLFW_MOUSE_BUTTON_RIGHT;
+            case "MOUSE_MIDDLE" -> GLFW.GLFW_MOUSE_BUTTON_MIDDLE;
+            case "MOUSE_4" -> 3;
+            case "MOUSE_5" -> 4;
+            case "MOUSE_6" -> 5;
+            case "MOUSE_7" -> 6;
+            case "MOUSE_8" -> 7;
+            default -> -1;
+        };
     }
 } 
--- src/main/java/net/prizowo/keyboardjs/keyboard/KeyboardEventJS.java
+++ src/main/java/net/prizowo/keyboardjs/keyboard/KeyboardEventJS.java
@@ -1,51 +1,16 @@
 package net.prizowo.keyboardjs.keyboard;
 
 import dev.latvian.mods.kubejs.event.EventJS;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.level.Level;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraftforge.server.ServerLifecycleHooks;
 
 public class KeyboardEventJS extends EventJS {
     private final String key;
     private final int keyValue;
     private final boolean pressed;
-    private final LocalPlayer clientPlayer;
-    private final ServerPlayer serverPlayer;
-    private final Level clientLevel;
-    private final ServerLevel serverLevel;
-    private final MinecraftServer server;
 
-    public KeyboardEventJS(String key, int keyValue, boolean pressed, LocalPlayer clientPlayer, Level clientLevel, ServerPlayer serverPlayer) {
+    public KeyboardEventJS(String key, int keyValue, boolean pressed) {
         this.key = key;
         this.keyValue = keyValue;
         this.pressed = pressed;
-        this.clientPlayer = clientPlayer;
-        this.clientLevel = clientLevel;
-        
-        if (serverPlayer != null) {
-            // 如果直接提供了服务器玩家，使用它
-            this.server = serverPlayer.getServer();
-            this.serverPlayer = serverPlayer;
-            this.serverLevel = serverPlayer.serverLevel();
-        } else if (clientPlayer != null) {
-            // 否则尝试从客户端玩家获取
-            this.server = ServerLifecycleHooks.getCurrentServer();
-            if (server != null) {
-                this.serverPlayer = server.getPlayerList().getPlayer(clientPlayer.getUUID());
-                this.serverLevel = serverPlayer != null ? serverPlayer.serverLevel() : null;
-            } else {
-                this.serverPlayer = null;
-                this.serverLevel = null;
-            }
-        } else {
-            this.server = null;
-            this.serverPlayer = null;
-            this.serverLevel = null;
-        }
     }
 
     public String getKey() {
@@ -60,30 +25,9 @@ public class KeyboardEventJS extends EventJS {
         return pressed;
     }
 
-    public LocalPlayer getClientPlayer() {
-        return clientPlayer;
-    }
-
-    public ServerPlayer getPlayer() {
-        return serverPlayer;
-    }
-
-    public Level getClientLevel() {
-        return clientLevel;
-    }
-
-    public ServerLevel getLevel() {
-        return serverLevel;
-    }
-
-    public MinecraftServer getServer() {
-        return server;
-    }
-
-    public boolean hasServerAccess() {
-        return server != null && serverPlayer != null && serverLevel != null;
-    }
-
+    /**
+     * 可用的按键枚举
+     */
     public enum Keys {
         A, B, C, D, E, F, G, H, I, J, K, L, M,
         N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
@@ -92,8 +36,7 @@ public class KeyboardEventJS extends EventJS {
         NUM_5, NUM_6, NUM_7, NUM_8, NUM_9,
         
         F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12,
-        F13, F14, F15, F16, F17, F18, F19, F20, F21, F22, F23, F24, F25,
-        
+
         ESCAPE, ENTER, TAB, BACKSPACE, INSERT, DELETE,
         RIGHT, LEFT, DOWN, UP, PAGE_UP, PAGE_DOWN,
         HOME, END, CAPS_LOCK, SCROLL_LOCK, NUM_LOCK,
@@ -106,27 +49,9 @@ public class KeyboardEventJS extends EventJS {
         KP_DECIMAL, KP_DIVIDE, KP_MULTIPLY, KP_SUBTRACT,
         KP_ADD, KP_ENTER, KP_EQUAL,
         
-        VOLUME_MUTE, VOLUME_UP, VOLUME_DOWN,
-        MEDIA_PLAY_PAUSE, MEDIA_STOP,
-        MEDIA_PREV_TRACK, MEDIA_NEXT_TRACK,
-        
-        BROWSER_BACK, BROWSER_FORWARD, BROWSER_REFRESH,
-        BROWSER_STOP, BROWSER_SEARCH, BROWSER_FAVORITES,
-        BROWSER_HOME,
-        
-        MAIL, CALCULATOR, FILE_EXPLORER, MEDIA_SELECT,
-        MY_COMPUTER, APP_1, APP_2,
-        
         SPACE, APOSTROPHE, COMMA, MINUS, PERIOD, SLASH,
         SEMICOLON, EQUAL, LEFT_BRACKET, BACKSLASH,
-        RIGHT_BRACKET, GRAVE_ACCENT, WORLD_1, WORLD_2,
-        
-        CONVERT, NON_CONVERT, YEN, KANJI,
-        
-        SECTION, MUTE,
-        POWER, SLEEP, WAKE, HELP, UNDO, REDO, NEW,
-        OPEN, CLOSE, REPLY, FORWARD, SEND, SPELL_CHECK,
-        TOGGLE_DICTATE, MIC, BRIGHTNESS_DOWN, BRIGHTNESS_UP,
+        RIGHT_BRACKET, GRAVE_ACCENT,
         
         MOUSE_LEFT, MOUSE_RIGHT, MOUSE_MIDDLE,
         MOUSE_4, MOUSE_5, MOUSE_6, MOUSE_7, MOUSE_8;
@@ -135,14 +60,5 @@ public class KeyboardEventJS extends EventJS {
         public String toString() {
             return name();
         }
-    }
-
-    public boolean is(String keyName) {
-        // 确保转换为大写
-        return this.keyValue == KeyMapping.getKeyCode(keyName.toUpperCase());
-    }
-
-    public boolean is(Keys key) {
-        return this.is(key.name());
     }
 } 
